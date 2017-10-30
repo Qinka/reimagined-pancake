fad0edb64b38edf66f5f42e3ac792db64c9502dd
Johann Lee <qinka@live.com>
2017-10-30 09:34:53 +0800

    update

--- mnist_simple/__init__.py
+++ mnist_simple/__init__.py
@@ -1,2 +1,2 @@
 from .model import Model
-from .train import train
+from .train import train, get_mnist
--- mnist_simple/train.py
+++ mnist_simple/train.py
@@ -1,5 +1,4 @@
-
-import model
+from mnist_simple import model
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
